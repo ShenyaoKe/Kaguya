0c32e15f1d84d11a4f0122fe5c1defbddb60ad8a
ShenyaoKe <akika@tamu.edu>
2016-03-22 15:32:41 -0500

    modified image transformer

--- viewer/ImageViewer.cpp
+++ viewer/ImageViewer.cpp
@@ -15,9 +15,6 @@ ImageViewer::~ImageViewer()
 
 void ImageViewer::setpixmap(const vector<uint8_t> &pixmap)
 {
-
 	QImage img(&pixmap[0], default_resX, default_resY, QImage::Format::Format_RGB888);
-	//img = img.transformed(QTransform().rotate(90));
-	ui.img_space->setPixmap(QPixmap::fromImage(img.transformed(QTransform().scale(1, -1))));
-	//update();
+	ui.img_space->setPixmap(QPixmap::fromImage(img.transformed(QTransform::fromScale(1, -1))));
 }
