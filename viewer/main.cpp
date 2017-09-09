1af5c7965fb4c05438f7a79a897c9392b29286f3
ShenyaoKe <akika.syk@gmail.com>
2017-09-09 10:22:32 -0700

    Check for DPI settings

--- viewer/main.cpp
+++ viewer/main.cpp
@@ -4,6 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION > QT_VERSION_CHECK(5, 5, 0)
+	qunsetenv("QT_DEVICE_PIXEL_RATIO");
+	qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", QByteArray("1"));
+#else
+	qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("0"));
+#endif
+
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication qtApp(argc, argv);
 	QFile File(":stylesheets/dark.qss");
